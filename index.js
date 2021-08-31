caeee997155138d03766c20af772a553a9ee0719
Aharon <aharonzarnighian@gmail.com>
2021-08-31 12:18:54 -0400

    Changed github logo stuff, and added vh fix

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // To make the GitHub image have the same height as the Trovare and Restaurant images.
-        // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-    var myWorkImages = document.querySelector('.myWorkImgsContainer > div > img');
-    var compStyles = window.getComputedStyle(myWorkImages);    
-    var myWorkImagesHeight = compStyles.getPropertyValue('height');
-    document.querySelector('.github div').style.height = myWorkImagesHeight;
+    // Fix for vh units on mobile:
+        // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    let vh100 = window.innerHeight;
+    document.documentElement.style.setProperty('--vh100', `${vh100}px`);            
+    
+    // Do the same thing if screen gets resized, like if device gets rotated
+    window.addEventListener('resize', () => {
+        let vh100 = window.innerHeight;
+        document.documentElement.style.setProperty('--vh100', `${vh100}px`);        
+    });        
 });
