4e60cef79d9f584707119556d32262a3812e91a5
Aharon <aharonzarnighian@gmail.com>
2021-09-02 12:13:27 -0400

    Styling and renaming

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Fix for vh units on mobile:
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-    let vh100 = window.innerHeight;
-    document.documentElement.style.setProperty('--vh100', `${vh100}px`);            
     
-    // Do the same thing if screen gets resized, like if device gets rotated
-    // window.addEventListener('resize', () => {
-    //     let vh100 = window.innerHeight;
-    //     document.documentElement.style.setProperty('--vh100', `${vh100}px`);        
-    // });        
+    let vh100 = window.innerHeight;
+    document.documentElement.style.setProperty('--vh100', `${vh100}px`);                       
 });
