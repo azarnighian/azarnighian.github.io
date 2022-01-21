36a2bd6c2c17dc683f6e5aecbe1680c44bd4d60f
Aharon <aharonzarnighian@gmail.com>
2022-01-20 22:15:04 -0500

    Fixed vhfix and myContactInfoContainer

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
     
     let vh100 = window.innerHeight;
-    document.documentElement.style.setProperty('--vh100', `${vh100}px`);                       
+    document.documentElement.style.setProperty('--vh100', `${vh100}px`);
+    
+    // Do the same thing if screen gets resized, like if device gets rotated
+    window.addEventListener('resize', () => {
+        let vh100 = window.innerHeight;
+        document.documentElement.style.setProperty('--vh100', `${vh100}px`);
+    });
 });
