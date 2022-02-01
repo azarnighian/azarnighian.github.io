5e42ce80948d2e73d3f4d0f4a789f2f1b460dd19
Aharon <aharonzarnighian@gmail.com>
2022-02-01 14:31:00 -0500

    Fixed vh on landing for mobile

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Fix for vh units on mobile:
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
     
-    let vh100 = window.innerHeight;
-    document.documentElement.style.setProperty('--vh100', `${vh100}px`);
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     
     // Do the same thing if screen gets resized, like if device gets rotated
     window.addEventListener('resize', () => {
-        let vh100 = window.innerHeight;
-        document.documentElement.style.setProperty('--vh100', `${vh100}px`);
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
 });
