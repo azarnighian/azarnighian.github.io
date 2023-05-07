33f9003d5fec3110d5692eb00392ca6f1627a790
Aharon <aaronz1296@gmail.com>
2023-05-07 13:04:18 -0400

    Removed wrapper and vh resize...

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
     
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    
-    // Do the same thing if screen gets resized, like if device gets rotated
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
+    document.documentElement.style.setProperty('--vh', `${vh}px`);        
 });
